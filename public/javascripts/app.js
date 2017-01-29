c57401dfe205e7d51b349767fb74a396ace00a0d
Ajay NS <ajay.ns@outlook.com>
2017-01-29 18:36:18 +0530

    Add ngAnimate page transitions

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ngResource']);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ngAnimate']);
 
 // Services
 app.service('Complaints', function ($resource) {
